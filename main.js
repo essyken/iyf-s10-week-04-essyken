a34489fe0bef4f497760623d6aafa986e2fd57d8
essyken <njorogeesther356@gmail.com>
2026-03-13 09:16:58 +0300

    main.js

--- main.js
+++ main.js
@@ -15,3 +15,14 @@ score = 150;  // Works!
 
 const PI = 3.14159;
 // PI = 3;  // Error! const cannot be reassigned
+
+letname = "Esther";  // Works!
+console.log(name);  // Output: Esther
+letage = 35;  // Works!
+console.log(age);  // Output: 35
+let isstudent = true;  // Works!
+console.log(isstudent);  // Output: true
+let favouritecolor = "blue,red,grey";  // Works!
+console.log(favouritecolor);  // Output: blue,red,grey
+let todaydate = "2026-03-12";  // Works!
+console.log(todaydate);  // Output: 2026-03-12
